0bd54a27f53f8e1da1662efb12e596ffb21393fe
jong sung kim <cwos17@naver.com>
2021-08-03 18:00:40 +0900

    시간역행

---
+++ Assets/Script/Bomb.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+
+    Rigidbody m_rigidBody;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        m_rigidBody = GetComponent<Rigidbody>();
+    }
+    void Start()
+    {
+        m_rigidBody.AddForce(Vector3.up * 300f);
+        m_rigidBody.AddForce(Vector3.left * 300f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Script/TimeBody.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TimeBody : MonoBehaviour
+{
+
+    public bool m_isRewinding = false;
+
+    List<Vector3> m_position;
+    Rigidbody rb;
+
+
+    void Start()
+    {
+        m_position = new List<Vector3>();
+        rb = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+            StartRewind();
+        if (Input.GetKeyUp(KeyCode.Return))
+            StopRewind();
+    }
+    void FixedUpdate()
+    {
+        if (m_isRewinding)
+            Rewind();
+        else
+            Record();
+    }
+    void Record()
+    {
+        m_position.Insert(0, transform.position);
+    }
+    void Rewind()
+    {
+        if (m_position.Count > 0)
+        {
+            transform.position = m_position[0];
+            m_position.RemoveAt(0);
+        }
+        else
+        {
+            StopRewind();
+        }
+    }
+    public void StartRewind()
+    {
+        m_isRewinding = true;
+       rb.isKinematic = true;
+    }
+    public void StopRewind()
+    {
+        m_isRewinding = false;
+        rb.isKinematic = false;
+    }
+}
+
